260be1e3706da3090f243caa504e695cc39aa1c9
Dmitriy Sychev <dmitriy.sychev@utexas.edu>
2020-12-26 17:15:04 -0600

    class name is optional

--- tag_parser.py
+++ tag_parser.py
@@ -8,6 +8,7 @@ import urllib.error as err
 
 # strict type specialization
 def getSoup(link: str):
+    response = None
     try:
         response = request.urlopen(link)
     except err.URLError as re:
@@ -19,9 +20,13 @@ def getSoup(link: str):
 
 # Method for parsing
 # returns a list of tags specified by the parameter 'html_tag'
-def parse(soup, html_tag, class_name):
+def parse(soup, html_tag, class_name = None):
     tags = []
-    for post_title in soup.findAll(html_tag, attrs={'class': class_name}): # tag='article', class_name = 'post post_preview'
+    attrs = {}
+    if class_name is not None:
+        attrs = {'class': class_name}
+        
+    for post_title in soup.findAll(html_tag, attrs): # tag='article', class_name = 'post post_preview'
         if __debug__: 
             print(post_title)
         tags.append(post_title)
