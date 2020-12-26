71d6a338b671b01bb616c395a275cc1e4570a4d1
Dmitriy Sychev <dmitriy.sychev@utexas.edu>
2020-12-26 16:52:00 -0600

    Site Parser by tag and class

---
+++ tag_parser.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import urllib.request as request
+import urllib.error as err
+
+# In Python there is a feature for simple print style debugging to be switched on or off.
+# If you run a Python script with the -O option then
+# python -O habr.py means run with debug == false
+
+# strict type specialization
+def getSoup(link: str):
+    try:
+        response = request.urlopen(link)
+    except err.URLError as re:
+        raise re
+
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
+# Method for parsing
+# returns a list of tags specified by the parameter 'html_tag'
+def parse(soup, html_tag, class_name):
+    tags = []
+    for post_title in soup.findAll(html_tag, attrs={'class': class_name}): # tag='article', class_name = 'post post_preview'
+        if __debug__: 
+            print(post_title)
+        tags.append(post_title)
+    return tags
+
