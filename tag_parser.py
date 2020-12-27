dd6eeeaea86254a22e44aaee7158ff0ff6b0897c
Dmitriy Sychev <dmitriy.sychev@utexas.edu>
2020-12-26 18:02:43 -0600

    added specific tag extractor

--- tag_parser.py
+++ tag_parser.py
@@ -6,6 +6,7 @@ import urllib.error as err
 # If you run a Python script with the -O option then
 # python -O habr.py means run with debug == false
 class Parser:
+
     def __init__(self, website):
         self.website = website
     
@@ -29,16 +30,16 @@ class Parser:
     # returns a list of tags specified by the parameter 'html_tag'
     '''
     def parse(self, soup, html_tag, class_name = None):
-        tags = []
         attrs = {}
+        self.tags = []
         if class_name is not None:
             attrs = {'class': class_name}
             
         for tag in soup.findAll(html_tag, attrs): # tag='article', class_name = 'post post_preview'
             if __debug__: 
                 print(tag)
-            tags.append(tag)
-        return tags
+            self.tags.append(tag)
+        return self.tags
 
     '''
     # In order to get what you want from the website, specify *tag* param
@@ -55,7 +56,21 @@ class Parser:
     def ppretty(self):
         print(self.getSoup(self.website).prettify())
 
+    '''
+    # Function to extract a specific tag from the
+    # list of tags we parsed
+    '''
+    def extractTag(self, tag, tagList):
+        specificTags = []
+        [specificTags.append(element.find(tag)) for element in tagList]
+        return specificTags
+
 # usage example:
 habr = Parser("https://habr.com/en/flows/develop/")
 print(habr)
 storedTags = habr.getListOfTags('article', 'post post_preview')
+
+particularTag = habr.extractTag('h2', storedTags)
+
+#for example this is how you would get a post title from this website
+print(particularTag[0].find('a').string)
